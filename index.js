ab3db5355a7742a85d3eb32dfbefee11ef29af59
Loukas Anastasiou <loukas@anastasiou.io>
2022-03-06 08:44:01 +0100

    Changed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const app = require('./server/server');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 require('dotenv').config();
 
 mongoose.connect(process.env.DEFAULT_CONNECTION, { useNewUrlParser: true })
