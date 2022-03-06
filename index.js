4c0fc4c5a4bb04bb4044e18057fdec5f0c478a13
Loukas Anastasiou <loukas@anastasiou.io>
2022-03-06 01:09:03 +0100

    Fixed indenting

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const app = require('./server/server');
 const port = process.env.PORT || 3000;
 
-mongoose.connect("mongodb+srv://***:***@cluster0.m6mqc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true })
+mongoose
+	.connect("mongodb+srv://***:***@cluster0.m6mqc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true })
 	.then(() => {
 		app.listen(port, () => {
 			console.log(`Server listening on port ${port}...`);
